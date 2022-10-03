3e70a8fc92145bff8af2e6e466213945e152171c
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-10-03 17:13:51 +0700

    model user, model transaksi, model food

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id', 'food_id', 'quantity', 'total', 'status', 'payment_url'
+    ];
+
+    public function food()
+    {
+        return $this->hasOne('Food::class', 'id', 'food_id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne('User::class', 'id', 'user_id');
+    }
+
+    public function getCreatedAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
 }
