b2514c7f50a5e62eb9ae2d13475bb9479542d8d8
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-10-13 17:08:48 +0700

    crud user edit dan update, crud makanan controller dan index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    use PasswordValidationRules;
     /**
      * Display a listing of the resource.
      *
@@ -55,7 +59,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
         //
     }
@@ -66,9 +70,11 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        return view('users.edit', [
+            'item' => $user
+        ]);
     }
 
     /**
@@ -78,9 +84,33 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $data = $request->all();
+        Validator::make($data, [
+            'name' => ['nullable', 'string', 'max:255'],
+            'email' => ['nullable', 'string', 'email', 'max:255', Rule::unique('users')->ignore(auth('sanctum')->user()->id)],
+            'password' => $this->passwordRules(),
+            'profile_photo_path' => ['image', 'max:2048'],
+            'address' => ['nullable', 'string'],
+            'roles' => ['string', 'max:255', 'in:USER,ADMIN'],
+            'houseNumber' => ['nullable', 'string', 'max:255'],
+            'phoneNumber' => ['nullable', 'string', 'max:255'],
+            'city' => ['nullable', 'string', 'max:255'],
+        ])->errors();
+
+        if ($request->file('profile_photo_path')) {
+            $data['profile_photo_path'] = $request->file('profile_photo_path')->store('assets/user', 'public');
+        }
+
+        if ($data['password']) {
+            $data['password'] = Hash::make($request->password);
+        } else {
+            $data['password'] = $user->password;
+        }
+
+        $user->update($data);
+        return redirect()->route('users.index');
     }
 
     /**
@@ -91,7 +121,7 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        if (!empty($user->profile_photo_path)) {
+        if ($user->profile_photo_path) {
             Storage::disk('public')->delete($user->profile_photo_path);
         }
         $user->delete();
